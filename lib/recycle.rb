eeca8edf1d5765cbccfe9817c12495f0f8a9463a
haileypressnell <hailey.pressnell@yahoo.com>
2018-07-16 18:58:46 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -1,7 +1,13 @@
 ###### Code Your Solutions Here!!!
 first_recycle_bin = {
-  
+  "address"=>"E 227 St/Bronx River Pkway",
+  "site_type"=>"Subproperty",
+  "longitude"=>"-73.864223918",
+  "borough"=>"Bronx",
+  "latitude"=>"40.890848989",
+  "park_site_name"=>"227th St. Plgd"
 }
+
 #Your code should work with a hash that looks like the one below (in comments).
 #The specs (which call your tests) are using these exact hashes 
 # We haven't hard coded the hashes in the tests, 
